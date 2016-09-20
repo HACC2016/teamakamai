e48467bb98c77f60539a25e9f9618269492f465f
Flaviu Chelaru <cfv1000@gmail.com>
2016-09-20 20:38:23 +0300

    updated twilio configurations

--- config/twilio.php
+++ config/twilio.php
@@ -1,11 +1,11 @@
 <?php
-    return [
-        // https://www.twilio.com/console
-        'ID' => 'AC617d64bed3f167abb7b8d9d635d21743',
-        'CONFIGURATION_ID' => 'VS481af4670bda93887c23a1780efe436d',
+return [
+    // https://www.twilio.com/console
+    'ID'               => env('TWILIO_ID', 'AC617d64bed3f167abb7b8d9d635d21743'),
+    'CONFIGURATION_ID' => env('TWILIO_CONFIG', 'VS481af4670bda93887c23a1780efe436d'),
 
-         // https://www.twilio.com/console/video/dev-tools/api-keys
-        'API' => 'SK6e84981d07885bc9df33e1ab043a2fb2',
-        'API_KEY' => 'wbTs1SUt6AjxtgeKeNCxuYvJa6PhaRd0'
-    ];
+    // https://www.twilio.com/console/video/dev-tools/api-keys
+    'API'              => env('TWILIO_API','SK6e84981d07885bc9df33e1ab043a2fb2'),
+    'API_KEY'          => env('TWILIO_API_KEY', 'wbTs1SUt6AjxtgeKeNCxuYvJa6PhaRd0')
+];
 ?>
