d07300c344207a900467dce51af3ab0227048788
Flaviu Chelaru <cfv1000@gmail.com>
2016-09-20 20:35:50 +0300

    removed telecare texts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="HandheldFriendly" content="true">
 
     <title ng-bind="$state.current.title" class="ng-binding">TEAM AKAMAI</title>
@@ -17,10 +18,14 @@
             <li><a class=" waves-effect waves-light" ui-sref="account:logout">Logout</a></li>
         </ul>
         <ul id="nav-mobile" class="side-nav">
-            <li><div class="userView">
-                    Telecare US
-                </div></li>
-            <li><div class="divider"></div></li>
+            <li>
+                <div class="userView">
+                    Team AKAMAI
+                </div>
+            </li>
+            <li>
+                <div class="divider"></div>
+            </li>
             <li><a ui-sref="account:logout" href="#">Logout</a></li>
         </ul>
         <a href="#" sidenav data-activates="nav-mobile" class="button-collapse"><i class="fa fa-list"></i></a>
@@ -41,7 +46,11 @@
         <div class="row">
             <div class="col l6 s12">
                 <h5 class="white-text">Company Bio</h5>
-                <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+                <p class="grey-text text-lighten-4">
+                    We are a team of college students working on this project like it's
+                    our full time job. Any amount would help support and continue development on this project and is
+                    greatly appreciated.
+                </p>
 
 
             </div>
@@ -72,12 +81,7 @@
     </div>
 </footer>
 
-@section('javascripts')
-    <script type="text/javascript">
-        var user          = {!! Auth::check() ? Auth::user() : 'false' !!};
-        var clientAddress = '{{url('/')}}:{{env('SOCKET_PORT')}}/';
-    </script>
-@show
+@section('javascripts') @show
 
 <script src="{{url('/')}}:{{env('SOCKET_PORT')}}/socket.io/socket.io.js"></script>
 <script type="text/javascript" src="{!! asset('assets/js/vendor.js') !!}"></script>
